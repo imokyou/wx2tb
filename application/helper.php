7b9f82862636bb6ded117ee194b616593882e38c
lupt <lupt@luptdeMacBook-Pro.local>
2017-10-10 12:00:47 +0800

    fix bug

--- application/helper.php
+++ application/helper.php
@@ -39,3 +39,60 @@ function curl_get($url, $timeout=60, $agent='', $cookie='')
     curl_close($fn);
     return $fm;
 }
+
+
+/**
+  * XML编码
+  * @param mixed $data 数据
+  * @param string $root 根节点名
+  * @param string $item 数字索引的子节点名
+  * @param string $attr 根节点属性
+  * @param string $id   数字索引子节点key转换的属性名
+  * @param string $encoding 数据编码
+  * @return string
+  */
+function xml_encode($data, $root='think', $item='item', $attr='', $id='id', $encoding='utf-8') {
+    if(is_array($attr)){
+        $_attr = array();
+        foreach ($attr as $key => $value) {
+            $_attr[] = "{$key}=\"{$value}\"";
+        }
+        $attr = implode(' ', $_attr);
+    }
+    $attr   = trim($attr);
+    $attr   = empty($attr) ? '' : " {$attr}";
+    $xml    = "<?xml version=\"1.0\" encoding=\"{$encoding}\"?>";
+    $xml   .= "<{$root}{$attr}>";
+    $xml   .= data_to_xml($data, $item, $id);
+    $xml   .= "</{$root}>";
+    return $xml;
+}
+ 
+/**
+  * 数据XML编码
+  * @param mixed  $data 数据
+  * @param string $item 数字索引时的节点名称
+  * @param string $id   数字索引key转换为的属性名
+  * @return string
+  */
+function data_to_xml($data, $item='item', $id='id') {
+    $xml = $attr = '';
+    foreach ($data as $key => $val) {
+        if(is_numeric($key)){
+            $id && $attr = " {$id}=\"{$key}\"";
+            $key  = $item;
+        }
+        $xml    .=  "<{$key}{$attr}>";
+        // xml 转义特殊字符 如& 以<![CDATA[标记开始，以]]>标记结束,必须是最终文本值才能加上这个<![CDATA[xxx]]>
+        $xml    .=  (is_array($val) || is_object($val)) ? data_to_xml($val, $item, $id) : '<![CDATA['.$val.']]>';
+        $xml    .=  "</{$key}>";
+    }
+    return $xml;
+}
+
+function xml_to_data($xml){
+    $obj  = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
+    $json = json_encode($obj);
+    $data  = json_decode($json, true);
+    return $data;
+}
--- application/index/controller/Msg.php
+++ application/index/controller/Msg.php
@@ -4,12 +4,26 @@ namespace app\index\controller;
 use think\Controller;
 use think\Request;
 use think\Response;
+use think\Log;
 
 class Msg extends Controller
 {
     public function index()
     {
-        $echostr = Request::instance()->get('echostr');
-        echo $echostr;
+        $xml = file_get_contents('php://input');
+        Log::record($xml, 'info');
+        if ($xml) {
+            $origin_data = xml_to_data($xml);
+
+            $data = array(
+                'ToUserName' => $origin_data['FromUserName'],
+                'FromUserName' => $origin_data['ToUserName'],
+                'CreateTime' => time(),
+                'MsgType' => 'text',
+                'Content' => '这是回复~'
+            );
+            return Response::create($data, 'xml')->code(200)->options(['root_node'=> 'xml']);
+        }
+        echo 'success';
     }
 }
